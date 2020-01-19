47dea983f4baa2aa8e3a0460cd5139c9e6ef8af5
Tony Trinh <tony.w.trinh3@gmail.com>
2020-01-18 23:44:34 -0800

    model

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Modal from '../Modal';
 
 const StreamDelete = () => {
-    return <div>StreamDelete</div>
+    // React Frament doesnt produce any thing on screen but works as div
+    const actions = (
+        <React.Fragment>
+            <button className="ui button negative ">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    )
+
+
+    return (<div>
+        StreamDelete
+       <Modal title = 'Delete Stream'
+       content = "Are you sure you want to delete this stream?"
+       actions={actions}
+       />
+    </div>
+    );
 };
 
 export default StreamDelete;
